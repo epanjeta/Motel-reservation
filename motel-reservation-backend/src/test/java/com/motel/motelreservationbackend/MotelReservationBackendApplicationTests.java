b4190a830db7eceb0099a3056ba32476964bcb4c
epanjeta <epanjeta1@etf.unsa.ba>
2023-02-08 23:32:11 +0100

    Initial commit

---
+++ motel-reservation-backend/src/test/java/com/motel/motelreservationbackend/MotelReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motel.motelreservationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
